61d0cb23ea33ccbc7786e6ebea3e11503b2f919b
Aotoda <116617731+Aotoda@users.noreply.github.com>
2023-06-22 16:20:50 +0700

    Delete bank directory

--- bank/src/main/java/com/example/bank/repo/DogRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bank.model.entities.Log;
-
-public interface DogRepo extends JpaRepository<Log,Integer>{
-
-}
--- bank/src/main/java/com/example/bank/repo/MySqlRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bank.model.entities.Client;
-
-public interface MySqlRepo extends JpaRepository<Client,Integer>{
-    
-}
